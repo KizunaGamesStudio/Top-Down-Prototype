0b3661c818a1920277976f0ae86b8489a7fa7de8
Nicole Peralta <89421724+nicolescania@users.noreply.github.com>
2023-12-04 16:13:38 -0500

    feature: if enemy collide with an object randomly move to another direction

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -6,7 +6,11 @@ public class FollowPlayer : MonoBehaviour
 {
 
     public float movementSpeed = 2.0f;
+    public float changeBehaviorCooldown = 2.0f; // Time in seconds for random movement
     private Transform player;
+    private bool isCollided = false;
+    private float behaviorTimer = 0.0f;
+    private Vector2 randomDirection; // Store the current random direction
 
     void Start()
     {
@@ -19,16 +23,55 @@ public class FollowPlayer : MonoBehaviour
 
     void Update()
     {
-        if (player != null)
+        if (!isCollided && player != null)
         {
-            Vector3 direction = player.position - transform.position;
-            direction.Normalize();
+            FollowPlayerMovement();
+        }
+        else
+        {
+            // If collided, change behavior for a certain cooldown period
+            behaviorTimer += Time.deltaTime;
 
-            transform.position += direction * movementSpeed * Time.deltaTime;
-
-            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Euler(0f, 0f, angle);
+            if (behaviorTimer < changeBehaviorCooldown)
+            {
+                MoveRandomly();
+            }
+            else
+            {
+                isCollided = false;
+                behaviorTimer = 0.0f;
+            }
         }
     }
 
+    void FollowPlayerMovement()
+    {
+        Vector3 direction = player.position - transform.position;
+        direction.Normalize();
+
+        transform.position += direction * movementSpeed * Time.deltaTime;
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, angle);
+    }
+
+    void MoveRandomly()
+    {
+        // Move in the current random direction
+        transform.position += (Vector3)randomDirection * movementSpeed * Time.deltaTime;
+
+        // Calculate angle for rotation (optional)
+        float angle = Mathf.Atan2(randomDirection.y, randomDirection.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, angle);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("ObjectsColliders"))
+        {
+            isCollided = true;
+            // Set a new random direction upon collision
+            randomDirection = Random.insideUnitCircle.normalized;
+        }
+    }
 }
