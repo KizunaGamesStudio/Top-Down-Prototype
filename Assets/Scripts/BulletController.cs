42dc44d2ed034bb1049d9d147215b800eba53a08
Nicole Peralta <89421724+nicolescania@users.noreply.github.com>
2023-12-06 15:35:07 -0500

    feature: enemigos cambian de color cuando se le disparan

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -21,10 +21,20 @@ public class BulletController : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
+
             EnemyController enemyControllerScript = collision.gameObject.GetComponent<EnemyController>();
+            SpriteRenderer enemySpriteRenderer = collision.gameObject.GetComponent<SpriteRenderer>();
 
             if (enemyControllerScript != null)
             {
+
+                // Convert hexadecimal color value to Color object
+                Color hexColor = HexToColor("#F89090"); // Replace "#FF0000" with your hexadecimal color value
+
+                // Set the enemy's color to the hexadecimal value
+                enemySpriteRenderer.color = hexColor;
+            
+
                 enemyControllerScript.TakeDamage(1);
                 Destroy(gameObject); // Destroy the bullet after hitting the enemy
             }
@@ -40,5 +50,11 @@ public class BulletController : MonoBehaviour
 
 
 
+        Color HexToColor(string hex)
+        {
+            Color color = new Color();
+            ColorUtility.TryParseHtmlString(hex, out color);
+            return color;
+        }
     }
 }
