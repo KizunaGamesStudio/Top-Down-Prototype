bcb91dc1e76274eed729424aa835845f38098f52
Micol Peralta <micolpa08@gmail.com>
2023-12-06 22:21:43 -0400

    fix: change enemy color on bullet collision

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -23,18 +23,9 @@ public class BulletController : MonoBehaviour
         {
 
             EnemyController enemyControllerScript = collision.gameObject.GetComponent<EnemyController>();
-            SpriteRenderer enemySpriteRenderer = collision.gameObject.GetComponent<SpriteRenderer>();
 
             if (enemyControllerScript != null)
             {
-
-                // Convert hexadecimal color value to Color object
-                Color hexColor = HexToColor("#F89090"); // Replace "#FF0000" with your hexadecimal color value
-
-                // Set the enemy's color to the hexadecimal value
-                enemySpriteRenderer.color = hexColor;
-            
-
                 enemyControllerScript.TakeDamage(1);
                 Destroy(gameObject); // Destroy the bullet after hitting the enemy
             }
@@ -43,18 +34,8 @@ public class BulletController : MonoBehaviour
         if (collision.gameObject.CompareTag("ObjectsColliders"))
         {
             Destroy(gameObject);
-          
-
-
         }
 
-
-
-        Color HexToColor(string hex)
-        {
-            Color color = new Color();
-            ColorUtility.TryParseHtmlString(hex, out color);
-            return color;
-        }
+        
     }
 }
--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -29,20 +29,41 @@ public class EnemyController : MonoBehaviour
     }
 
 
-
-
-
     public void TakeDamage(float damageAmount)
     {
         health -= damageAmount;
         HealthBarScript.UpdateHealthBar(health, maxHealth);
+        StartCoroutine(ChangeColor());
 
         if (health <= 0)
         {
             Destroy(gameObject);
-           
-
         }
+    }
+
+
+    IEnumerator ChangeColor()
+    {
+        SpriteRenderer enemySpriteRenderer = GetComponent<SpriteRenderer>();
+
+        // Convert hexadecimal color value to Color object
+        Color hexColor = HexToColor("#F89090"); // Replace "#FF0000" with your hexadecimal color value
+        
+        //Get Original color
+        Color originalColor = enemySpriteRenderer.color;
+
+        // Set the enemy's color to the hexadecimal value
+        enemySpriteRenderer.color = hexColor;
+
+        yield return new WaitForSeconds(0.1f);
+        enemySpriteRenderer.color = originalColor; // Return to original color
+    }
+
+    Color HexToColor(string hex)
+    {
+        Color color = new Color();
+        ColorUtility.TryParseHtmlString(hex, out color);
+        return color;
     }
 
 
