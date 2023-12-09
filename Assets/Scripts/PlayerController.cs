4493988a669ca97de8b98308412433c74953cb99
Micol Peralta <micolpa08@gmail.com>
2023-12-09 11:32:46 -0400

    bugfix/fix-shoot-controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,85 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 5f; // Adjust as needed
 
-    private Camera mainCamera;
-    private float playerWidth, playerHeight;
-    public Transform bulletSpawnPoint;
+
+    private float bulletSpeed = 15f;
     public GameObject bulletPrefab;
-    public float bulletSpedd = 10;
-    private Transform aimTransform;
-    public float bulletSpeed = 30f;
-
-
 
     // Start is called before the first frame update
-    private Rigidbody2D rb;
-
-    private void Start()
+    void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-
-        mainCamera = Camera.main;
-        aimTransform = transform.Find("BulletSpawnPoint");
-
-
-        // Assuming the player has a SpriteRenderer component
-        SpriteRenderer playerSprite = GetComponent<SpriteRenderer>();
-        playerWidth = playerSprite.bounds.extents.x;
-        playerHeight = playerSprite.bounds.extents.y;
+        
     }
+
     // Update is called once per frame
     void Update()
     {
 
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-
-        Vector3 movement = new Vector3(horizontalInput, verticalInput, 0f) * moveSpeed * Time.deltaTime;
-        Vector3 newPosition = transform.position + movement;
-
-        float distance = newPosition.z - mainCamera.transform.position.z;
-        float leftBound = mainCamera.ScreenToWorldPoint(new Vector3(0, 0, distance)).x + playerWidth;
-        float rightBound = mainCamera.ScreenToWorldPoint(new Vector3(Screen.width, 0, distance)).x - playerWidth;
-        float bottomBound = mainCamera.ScreenToWorldPoint(new Vector3(0, 0, distance)).y + playerHeight;
-        float topBound = mainCamera.ScreenToWorldPoint(new Vector3(0, Screen.height, distance)).y - playerHeight;
-
-        newPosition.x = Mathf.Clamp(newPosition.x, leftBound, rightBound);
-        newPosition.y = Mathf.Clamp(newPosition.y, bottomBound, topBound);
-
-        transform.position = newPosition;
-
-
-
-        Vector3 mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
-        Vector3 aimDirection = (mousePosition - transform.position).normalized;
-
-        float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
-        aimTransform.rotation = Quaternion.Euler(0f, 0f, angle);
-
+        //Shoot on mouse click and on space key down
         if (Input.GetKeyUp(KeyCode.Space) || Input.GetMouseButtonDown(0))
         {
-            ShootBullet();
+            Shoot();
         }
-
-
-  
-
-
     }
 
-    void ShootBullet()
-    {
-        GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
-        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+    void Shoot(){
+        
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-        // Set the velocity based on the direction the bullet should travel
-        rb.velocity = bulletSpawnPoint.up * bulletSpeed;
+		Vector2 direction = (Vector2)((mousePosition - transform.position));
+		direction.Normalize ();
+
+		// Creates the bullet locally
+		GameObject bullet = (GameObject)Instantiate (
+			                    bulletPrefab,
+								transform.position + (Vector3)( direction * 0.5f),
+			                    Quaternion.identity);
+
+		// Adds velocity to the bullet
+		bullet.GetComponent<Rigidbody2D> ().velocity = direction * bulletSpeed;
     }
-
 }
