fbed600af10937017a4d17edd331fad15e5417ff
Micol Peralta <micolpa08@gmail.com>
2023-12-20 20:38:47 -0400

    feature: add shooting effects

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using Assets.HeroEditor4D.Common.Scripts.CharacterScripts;
 using Assets.HeroEditor4D.Common.Scripts.Enums;
 using UnityEngine;
+using Assets.HeroEditor4D.Common.Scripts.ExampleScripts;
 
 public class PlayerController : MonoBehaviour
 {
 
 
-    private float bulletSpeed = 15f;
-    public GameObject bulletPrefab;
+   
+
+    Shoot shootScript;
 
 
     // Update is called once per frame
@@ -18,7 +20,7 @@ public class PlayerController : MonoBehaviour
         //Shoot on mouse click and on space key down
         if (Input.GetKeyUp(KeyCode.Space) || Input.GetMouseButtonDown(0))
         {
-            Shoot();
+            shootScript.ShootProjectile();
             //RotatePlayer();
         }
     }
@@ -27,20 +29,6 @@ public class PlayerController : MonoBehaviour
     
 
     void Shoot(){
-        
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-		Vector2 direction = (Vector2)((mousePosition - transform.position));
-		direction.Normalize ();
-
-		// Creates the bullet locally
-		GameObject bullet = (GameObject)Instantiate (
-			                    bulletPrefab,
-								transform.position + (Vector3)( direction * 0.5f),
-			                    Quaternion.identity);
-
-		// Adds velocity to the bullet
-		bullet.GetComponent<Rigidbody2D> ().velocity = direction * bulletSpeed;
     }
 
   
---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,65 @@
+using System.Linq;
+using Assets.HeroEditor4D.Common.Scripts.CharacterScripts;
+using Assets.HeroEditor4D.Common.Scripts.Enums;
+using UnityEngine;
+
+namespace Assets.HeroEditor4D.Common.Scripts.ExampleScripts
+{
+	/// <summary>
+	/// An example of how to handle user input, play animations and move a character.
+	/// </summary>
+	public class Shoot : MonoBehaviour
+	{
+        public Character4D Character;
+        public FirearmFxExample FirearmFx;
+
+         private float bulletSpeed = 15f;
+        public GameObject bulletPrefab;
+
+        public void Start()
+        {
+            Debug.Log("Here2");
+          
+        }
+
+        public void Update()
+        {
+            if (Input.GetKeyUp(KeyCode.Space) || Input.GetMouseButtonDown(0)){
+                ShootProjectile();
+                ShowShootFx();
+            }
+        }
+
+      
+        public void ShootProjectile(){
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            Vector2 direction = (Vector2)((mousePosition - transform.position));
+            direction.Normalize ();
+
+            // Creates the bullet locally
+            GameObject bullet = (GameObject)Instantiate (
+                                    bulletPrefab,
+                                    transform.position + (Vector3)( direction * 0.5f),
+                                    Quaternion.identity);
+
+            // Adds velocity to the bullet
+            bullet.GetComponent<Rigidbody2D> ().velocity = direction * bulletSpeed;
+        }
+       
+
+        private void ShowShootFx()
+        {
+            var firearm = Character.SpriteCollection.Firearm1H.SingleOrDefault(i => i.Sprites.Contains(Character.Parts[0].PrimaryWeapon))
+                ?? Character.SpriteCollection.Firearm2H.SingleOrDefault(i => i.Sprites.Contains(Character.Parts[0].PrimaryWeapon));
+
+            if (firearm != null)
+            {
+                Debug.Log("not null");
+                FirearmFx.CreateFireMuzzle(firearm.Name, firearm.Collection);
+            }
+        }
+
+
+	}
+}
