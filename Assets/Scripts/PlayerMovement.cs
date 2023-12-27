400709a4138f5be63da44a44340e4a616e75c4d1
Nicole Peralta <89421724+nicolescania@users.noreply.github.com>
2023-12-26 22:50:37 -0500

    fix: added bounds limits to the player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,12 +9,19 @@ public class PlayerMovement : MonoBehaviour
     public Character4D Character;
     public bool InitDirection;
     public int MovementSpeed;
+    private Camera mainCamera;
+    private float playerWidth, playerHeight;
+
 
     private bool _moving;
         Vector2 rotationDirection;
 
     public void Start()
+
+
     {
+        mainCamera = Camera.main;
+
         Character.AnimationManager.SetState(CharacterState.Ready);
 
         if (InitDirection)
@@ -30,6 +37,9 @@ public class PlayerMovement : MonoBehaviour
         //ChangeState();
         //Actions();
         //RotatePlayer();
+        MoveInsideBounds();
+
+
 
         if (Input.GetKeyDown(KeyCode.R))
         {
@@ -37,6 +47,36 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+
+
+
+     private void MoveInsideBounds()
+    {
+        SpriteRenderer playerSprite = GetComponent<SpriteRenderer>();
+        playerWidth = playerSprite.bounds.extents.x;
+        playerHeight = playerSprite.bounds.extents.y;
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontalInput, verticalInput, 0f) * 0.2f * Time.deltaTime;
+        Vector3 newPosition = transform.position + movement;
+
+        float distance = newPosition.z - mainCamera.transform.position.z;
+        float leftBound = mainCamera.ScreenToWorldPoint(new Vector3(0, 0, distance)).x + playerWidth;
+        float rightBound = mainCamera.ScreenToWorldPoint(new Vector3(Screen.width, 0, distance)).x - playerWidth;
+        float bottomBound = mainCamera.ScreenToWorldPoint(new Vector3(0, 0, distance)).y + playerHeight;
+        float topBound = mainCamera.ScreenToWorldPoint(new Vector3(0, Screen.height, distance)).y - playerHeight;
+
+        newPosition.x = Mathf.Clamp(newPosition.x, leftBound, rightBound);
+        newPosition.y = Mathf.Clamp(newPosition.y, bottomBound, topBound);
+
+        transform.position = newPosition;
+
+
+
+        Vector3 mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 aimDirection = (mousePosition - transform.position).normalized;
+    }
     private void SetDirection()
     {
         Vector2 direction;
